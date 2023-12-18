2bb85e74c243d45f014198c58fe0a75b4171ce8c
Ahmed Omar <ahmedomar2000.ao@gamil.com>
2023-12-18 06:29:30 +0200

    Initial commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-const dbUrL = 'mongodb://127.0.0.1:27017/yelp_camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 main().catch(err => console.log(err));
 
